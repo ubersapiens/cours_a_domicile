d44b38663924026b81f5fd7e10ef7558512cabdf
Juan Pardini <jfpardini@gmail.com>
2015-11-30 12:47:47 +0100

    Fix.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,7 +7,7 @@ class WelcomeController < ApplicationController
     @activity_groups2 = ActivityGroup.limit(4).offset(2)
     @activity_groups3 = ActivityGroup.limit(3).offset(6)
     @activity_groups4 = ActivityGroup.limit(4).offset(9)
-    @activity_groups5 = ActivityGroup.limit(1).offset(12)
+    @activity_groups5 = ActivityGroup.limit(1).offset(13)
   end
   def comment_ca_marche
     
