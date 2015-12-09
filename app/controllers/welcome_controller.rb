9f14cd54466ab5363b3d8c2bbcb74040b62fbf70
Juan Pardini <jfpardini@gmail.com>
2015-12-09 15:41:51 +0100

    Carousel fixes.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,12 +3,12 @@ class WelcomeController < ApplicationController
     @student = Student.new
     @gift = Gift.new
     @newsletter = Newsletter.new
-    @activity_groups1 = ActivityGroup.first(4)
-    @activity_groups2 = ActivityGroup.limit(4).offset(2)
-    @activity_groups3 = ActivityGroup.limit(3).offset(6)
-    @activity_groups4 = ActivityGroup.limit(4).offset(9)
-    @activity_groups5 = ActivityGroup.limit(1).offset(13)
-    @activity_groups6 = ActivityGroup.limit(3).offset(9)
+    @activity_groups1 = ActivityGroup.where(id: [1,2,3,4])
+    @activity_groups2 = ActivityGroup.where(id: [5,6,7,8])
+    @activity_groups3 = ActivityGroup.where(id: [9,10,11,12])
+    @activity_groups4 = ActivityGroup.where(id: [13,14])
+    @activity_groups5 = ActivityGroup.where(id: [15,16,17,18])
+    @activity_groups6 = ActivityGroup.where(id: [19,20])
   end
   def comment_ca_marche
     
